0013d34dc517db0167e29fa9a5ed007f914a1ed8
DauntlessRony <ronyjnu85@gmail.com>
2023-03-18 21:25:50 +0600

    Date and Time of Commit: 03/18/2023 21:25:50

--- demo.py
+++ demo.py
@@ -1,1 +1,29 @@
-#-------------------------------------------------------------------
+# prime numbers
+def isPrime(x):
+    if x < 2:
+        return False
+        print(f'{x} is not a prime number.')
+    elif x == 2:
+        return True
+        print(f'{x} is a prime number.')
+    for n in range(2, x):
+        if x % n ==0:
+            return False
+            print(f'{x} is not a prime number.')
+    return True
+    print(f'{x} is a prime number.')
+
+def primeGenerator(a, b):
+    for i in range(a,b):
+        print(f'The value of i before yield call {i}.')
+        #yield i
+        print(f'The value of i after yield call {i}.')
+        print(isPrime(i))
+        print(f'The value of i after call isPrime{i}.')
+        i += 1
+
+# f = int(input())
+# t = int(input())
+
+# print(list(primeGenerator(f, t)))
+primeGenerator(10, 20)
